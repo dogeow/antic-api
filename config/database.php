3e7be9a8ab8b20125b47c2ab6c07b79d05675b56
likunyan <lky216@gmail.com>
2020-03-07 20:13:10 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -93,8 +93,8 @@ return [
         'testing' => [
             'driver' => 'mysql',
             'host' => env('DB_TEST_HOST', 'localhost'),
-            'database' => env('DB_TEST_DATABASE', 'homestead'),
-            'username' => env('DB_TEST_USERNAME', 'homestead'),
+            'database' => env('DB_TEST_DATABASE', 'testing'),
+            'username' => env('DB_TEST_USERNAME', 'testing'),
             'password' => env('DB_TEST_PASSWORD', 'secret'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
