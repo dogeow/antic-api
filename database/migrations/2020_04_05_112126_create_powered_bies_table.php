afd6af297d1a31546df9f467660b1a5931587fc9
likunyan <lky216@gmail.com>
2020-04-05 17:12:03 +0800

    Update 2020_04_05_112126_create_powered_bies_table.php

--- database/migrations/2020_04_05_112126_create_powered_bies_table.php
+++ database/migrations/2020_04_05_112126_create_powered_bies_table.php
@@ -14,6 +14,7 @@ class CreatePoweredBiesTable extends Migration
     public function up()
     {
         Schema::create('powered_bies', function (Blueprint $table) {
+            $table->bigIncrements('id');
             $table->string('name', 100);
             $table->string('category', 100);
             $table->string('link')->nullable();
