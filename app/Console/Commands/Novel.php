4544e215797cfb8f0367a2c0528af18ceb316e57
likunyan <lky216@gmail.com>
2020-05-25 17:02:25 +0800

    233

---
+++ app/Console/Commands/Novel.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client as GuzzleClient;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Novel extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'novel';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '爬取小说';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $url = 'http://www.dingdianxs.com/5/5155/';
+        $client = new GuzzleClient();
+        $response = $client->request('GET', $url);
+        $content = $response->getBody()->getContents();
+        $crawler = new Crawler($content);
+        $lists = $crawler->filterXPath("//div[@id='list']/dl/dd")->each(function (Crawler $node) {
+            if (0 === $node->filterXPath('//dd/a')->count()) {
+                return false;
+            }
+
+            $name = $node->filterXPath('//dd/a')->text();
+            $url = $node->filterXPath("//dd/a/@href")->text();
+
+            return compact('name', 'url');
+
+        });
+
+        $lists = array_filter($lists);
+
+        foreach ($lists as $list) {
+            $pageUrl = $url.$list['url'];
+            $response = $client->request('GET', $pageUrl);
+            $content = $response->getBody()->getContents();
+            $crawler = new Crawler($content);
+            $txt = br2nl($crawler->filterXPath("//div[@id='content']")->html());
+            file_put_contents('novel/'.$list['name'].'.txt', $txt);
+        }
+    }
+}
