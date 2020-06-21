857f02304b4777df48a8d7ea9cdf7cd8ae18027a
likunyan <lky216@gmail.com>
2020-06-21 10:48:20 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::any('callback', function (Illuminate\Support\Facades\Request $request) {
-    file_put_contents('../233.log', var_export($request->all(), 1).PHP_EOL, FILE_APPEND);
+Route::post('callback', function () {
+    file_put_contents('../233.log', var_export($_POST, 1).PHP_EOL, FILE_APPEND);
 });
