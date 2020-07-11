daa0e9da590f265376081fc6fbe6baa9ccfda1fb
likunyan <lky216@gmail.com>
2020-07-11 23:43:52 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
-Route::post('callback', function(\Illuminate\Http\Request $request){
+Route::post('callback', function (Illuminate\Http\Request $request) {
     file_put_contents('../233.log', var_export($request->all(), 1).PHP_EOL, FILE_APPEND);
 });
+
