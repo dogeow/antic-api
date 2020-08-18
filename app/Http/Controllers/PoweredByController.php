d14efc72dfede50403f515b9757955d7d6bb8739
likunyan <lky216@gmail.com>
2020-08-18 19:10:27 +0800

    Update PoweredByController.php

--- app/Http/Controllers/PoweredByController.php
+++ app/Http/Controllers/PoweredByController.php
@@ -7,8 +7,16 @@ use Illuminate\Http\Request;
 
 class PoweredByController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return PoweredBy::jsonPaginate(request('size'));
+        if ($request->search === null || count($request->search) !== 1) {
+            return PoweredBy::jsonPaginate(request('size'));
+        }
+
+        foreach ($request->search as $key => $value) {
+            if (in_array($key, ['name']) && $value !== '') {
+                return PoweredBy::where($key, 'like', '%'.$value.'%')->jsonPaginate(request('size'));
+            }
+        }
     }
 }
