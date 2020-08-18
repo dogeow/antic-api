64c2d6704aebc6eb9a4eb3fc92f0d846512c8333
likunyan <lky216@gmail.com>
2020-08-18 19:54:14 +0800

    Update PoweredByController.php

--- app/Http/Controllers/PoweredByController.php
+++ app/Http/Controllers/PoweredByController.php
@@ -4,19 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\PoweredBy;
 use Illuminate\Http\Request;
+use PhpParser\Node\Expr\AssignOp\Pow;
+use Illuminate\Support\Facades\DB;
 
 class PoweredByController extends Controller
 {
     public function index(Request $request)
     {
-        if ($request->search === null || count($request->search) !== 1) {
-            return PoweredBy::jsonPaginate(request('size'));
+        $searchAble = ['name', 'category', 'note', 'link'];
+        $query = PoweredBy::query();
+        if ($request->search !== null) {
+            $query->where(DB::raw("concat_ws(name, note, link)"), 'like', '%'.$request->search.'%');
         }
-
-        foreach ($request->search as $key => $value) {
-            if (in_array($key, ['name']) && $value !== '') {
-                return PoweredBy::where($key, 'like', '%'.$value.'%')->jsonPaginate(request('size'));
+        if ($request->filters !== null) {
+            foreach ($request->filters as $key => $value) {
+                if (in_array($key, $searchAble) && $value !== '') {
+                    $query->where($key, 'like', '%'.$value.'%');
+                }
             }
         }
+
+
+        return $query->jsonPaginate(request('size'));
     }
 }
