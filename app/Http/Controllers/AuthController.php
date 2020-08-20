6ea40135d667df834cb18fbaef2f817aaa63fefe
likunyan <lky216@gmail.com>
2020-08-20 16:20:49 +0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,10 +26,10 @@ class AuthController extends Controller
         $rules = [
             'name' => [
                 'required', 'not_regex:/\s+/', function ($attribute, $value, $fail) {
-                    if (mb_strwidth($value) < 4 || mb_strwidth($value) > 16 ) {
+                    if (mb_strwidth($value) < 4 || mb_strwidth($value) > 16) {
                         $fail('昵称 宽度必须在 4 - 16 之间（一个中文文字为 2 个宽度）');
                     }
-                },],
+                }, ],
             'email' => ['required', 'not_regex:/\s+/', 'email', 'unique:users'],
             'password' => ['required', 'not_regex:/\s+/', 'min:8', 'max:16'],
             'password_confirmation' => ['same:password'],
