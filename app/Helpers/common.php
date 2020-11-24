26cdd9108034f315ef00c9b28d9d8ad1debd7a8d
likunyan <lky216@gmail.com>
2020-11-24 17:06:14 +0800

    Update common.php

--- app/Helpers/common.php
+++ app/Helpers/common.php
@@ -2,6 +2,6 @@
 
 function br2nl($input)
 {
-    return preg_replace('/<br\s?\/?>/ius', "\n",
+    return preg_replace('/<br\s?\/?>/iu', "\n",
         str_replace("\n", '', str_replace("\r", '', htmlspecialchars_decode($input))));
 }
