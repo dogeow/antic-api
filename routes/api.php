b38f1f5aed6e88fb52b48a5dc66836d598889876
likunyan <lky216@gmail.com>
2020-12-28 23:16:09 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -89,7 +89,7 @@ Route::group(['middleware' => 'api'], function ($router) {
     Route::get('/tags', [PostTagController::class, 'index']);
 
     // PHP 函数
-    Route::get('/php-function', [PHPFunctionController::class, 'index']);
+    Route::post('/php-function', [PHPFunctionController::class, 'index']);
 
     Route::group([
         'prefix' => 'user',
