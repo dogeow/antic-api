9fa777387b61ea78dfd27b37423f4abf7d1421c7
likunyan <lky216@gmail.com>
2021-01-22 23:47:05 +0800

    parking

---
+++ app/GraphQL/Queries/Parking.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+class Parking
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     * @return array
+     */
+    public function __invoke($_, array $args): array
+    {
+        return (new  \App\Http\Controllers\ApiController)->parking();
+    }
+}
