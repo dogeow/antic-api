a27dd466df1d7fbc9e871b4540b9152fd22de0f7
likunyan <lky216@gmail.com>
2021-01-28 17:27:15 +0800

    Update OSS.php

--- app/Services/OSS.php
+++ app/Services/OSS.php
@@ -2,14 +2,12 @@
 
 namespace App\Services;
 
-use JohnLui\AliyunOSS;
-
-use Exception;
 use DateTime;
+use Exception;
+use JohnLui\AliyunOSS;
 
 class OSS
 {
-
     /* 城市名称：
      *
      *  经典网络下可选：杭州、上海、青岛、北京、张家口、深圳、香港、硅谷、弗吉尼亚、新加坡、悉尼、日本、法兰克福、迪拜
@@ -23,13 +21,13 @@ class OSS
     private $ossClient;
 
     /**
-     * 私有初始化 API，非 API，不用关注
-     * @param  boolean 是否使用内网
+     * 私有初始化 API，非 API，不用关注.
+     * @param  bool 是否使用内网
      */
     public function __construct($isInternal = false)
     {
-        if ($this->networkType === 'VPC' && !$isInternal) {
-            throw new Exception("VPC 网络下不提供外网上传、下载等功能");
+        if ($this->networkType === 'VPC' && ! $isInternal) {
+            throw new Exception('VPC 网络下不提供外网上传、下载等功能');
         }
 
         $this->ossClient = AliyunOSS::boot(
@@ -41,100 +39,96 @@ class OSS
         );
     }
 
-
     /**
-     * 使用外网上传文件
+     * 使用外网上传文件.
      * @param  string bucket名称
      * @param  string 上传之后的 OSS object 名称
      * @param  string 上传文件路径
-     * @return boolean 上传是否成功
+     * @return bool 上传是否成功
      */
     public static function publicUpload($bucketName, $ossKey, $filePath, $options = [])
     {
-        $oss = new OSS();
+        $oss = new self();
         $oss->ossClient->setBucket($bucketName);
 
         return $oss->ossClient->uploadFile($ossKey, $filePath, $options);
     }
 
     /**
-     * 使用阿里云内网上传文件
+     * 使用阿里云内网上传文件.
      * @param  string bucket名称
      * @param  string 上传之后的 OSS object 名称
      * @param  string 上传文件路径
-     * @return boolean 上传是否成功
+     * @return bool 上传是否成功
      */
     public static function privateUpload($bucketName, $ossKey, $filePath, $options = [])
     {
-        $oss = new OSS(true);
+        $oss = new self(true);
         $oss->ossClient->setBucket($bucketName);
 
         return $oss->ossClient->uploadFile($ossKey, $filePath, $options);
     }
 
-
     /**
-     * 使用外网直接上传变量内容
+     * 使用外网直接上传变量内容.
      * @param  string bucket名称
      * @param  string 上传之后的 OSS object 名称
      * @param  string 上传的变量
-     * @return boolean 上传是否成功
+     * @return bool 上传是否成功
      */
     public static function publicUploadContent($bucketName, $ossKey, $content, $options = [])
     {
-        $oss = new OSS();
+        $oss = new self();
         $oss->ossClient->setBucket($bucketName);
 
         return $oss->ossClient->uploadContent($ossKey, $content, $options);
     }
 
     /**
-     * 使用阿里云内网直接上传变量内容
+     * 使用阿里云内网直接上传变量内容.
      * @param  string bucket名称
      * @param  string 上传之后的 OSS object 名称
      * @param  string 上传的变量
-     * @return boolean 上传是否成功
+     * @return bool 上传是否成功
      */
     public static function privateUploadContent($bucketName, $ossKey, $content, $options = [])
     {
-        $oss = new OSS(true);
+        $oss = new self(true);
         $oss->ossClient->setBucket($bucketName);
 
         return $oss->ossClient->uploadContent($ossKey, $content, $options);
     }
 
-
     /**
-     * 使用外网删除文件
+     * 使用外网删除文件.
      * @param  string bucket名称
      * @param  string 目标 OSS object 名称
-     * @return boolean 删除是否成功
+     * @return bool 删除是否成功
      */
     public static function publicDeleteObject($bucketName, $ossKey)
     {
-        $oss = new OSS();
+        $oss = new self();
         $oss->ossClient->setBucket($bucketName);
 
         return $oss->ossClient->deleteObject($bucketName, $ossKey);
     }
 
     /**
-     * 使用阿里云内网删除文件
+     * 使用阿里云内网删除文件.
      * @param  string bucket名称
      * @param  string 目标 OSS object 名称
-     * @return boolean 删除是否成功
+     * @return bool 删除是否成功
      */
     public static function privateDeleteObject($bucketName, $ossKey)
     {
-        $oss = new OSS(true);
+        $oss = new self(true);
         $oss->ossClient->setBucket($bucketName);
 
         return $oss->ossClient->deleteObject($bucketName, $ossKey);
     }
 
-
     /**
-     * -------------------------------------------------
+     * -------------------------------------------------.
      *
      *
      *  下面不再分公网内网出 API，也不注释了，大家自行体会吧。。。
@@ -142,17 +136,16 @@ class OSS
      *
      * -------------------------------------------------
      */
-
     public function copyObject($sourceBuckt, $sourceKey, $destBucket, $destKey)
     {
-        $oss = new OSS();
+        $oss = new self();
 
         return $oss->ossClient->copyObject($sourceBuckt, $sourceKey, $destBucket, $destKey);
     }
 
     public function moveObject($sourceBuckt, $sourceKey, $destBucket, $destKey)
     {
-        $oss = new OSS();
+        $oss = new self();
 
         return $oss->ossClient->moveObject($sourceBuckt, $sourceKey, $destBucket, $destKey);
     }
@@ -160,7 +153,7 @@ class OSS
     // 获取公开文件的 URL
     public static function getPublicObjectURL($bucketName, $ossKey)
     {
-        $oss = new OSS();
+        $oss = new self();
         $oss->ossClient->setBucket($bucketName);
 
         return $oss->ossClient->getPublicUrl($ossKey);
@@ -169,7 +162,7 @@ class OSS
     // 获取私有文件的URL，并设定过期时间，如 \DateTime('+1 day')
     public static function getPrivateObjectURLWithExpireTime($bucketName, $ossKey, DateTime $expire_time)
     {
-        $oss = new OSS();
+        $oss = new self();
         $oss->ossClient->setBucket($bucketName);
 
         return $oss->ossClient->getUrl($ossKey, $expire_time);
@@ -177,23 +170,22 @@ class OSS
 
     public static function createBucket($bucketName)
     {
-        $oss = new OSS();
+        $oss = new self();
 
         return $oss->ossClient->createBucket($bucketName);
     }
 
     public static function getAllObjectKey($bucketName)
     {
-        $oss = new OSS();
+        $oss = new self();
 
         return $oss->ossClient->getAllObjectKey($bucketName);
     }
 
     public static function getObjectMeta($bucketName, $ossKey)
     {
-        $oss = new OSS();
+        $oss = new self();
 
         return $oss->ossClient->getObjectMeta($bucketName, $ossKey);
     }
-
 }
