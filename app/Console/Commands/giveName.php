8c802436e002147954d5b8d8b7c0d2fce2a63fa7
likunyan <lky216@gmail.com>
2021-01-29 15:17:25 +0800

    Create giveName.php

---
+++ app/Console/Commands/giveName.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
+
+class giveName extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'giveName {sex}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '爬取名网';
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    private $concurrency = 50;  // 同时并发抓取
+    private $totalPageCount = 1e1000;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->client = new Client([
+            'timeout' => 10,
+        ]);
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $sex = $this->argument('sex');
+
+        $requests = static function ($total) use ($sex) {
+            $uri = 'https://www.quming.com/quming/';
+            for ($i = 0; $i < $total; $i++) {
+                yield new Request('POST', $uri, [
+                    'form_params' => [
+                        'gsname' => '',
+                        'sex' => $sex,
+                        'action' => 'test',
+                    ],
+                ]);
+            }
+        };
+
+        $pool = new Pool($this->client, $requests($this->totalPageCount), [
+            'concurrency' => $this->concurrency,
+            'fulfilled' => function ($response, $index) {
+                $this->info("请求第 $index 个请求");
+                $content = $response->getBody()->getContents();
+
+                if (preg_match_all('/rel="nofollow">(.*?)</', $content, $matches)) {
+                    $query = '';
+                    foreach ($matches[1] as $name) {
+                        $query .= "INSERT IGNORE INTO test (name, sex) VALUES ('$name', {$this->argument('sex')});";
+                    }
+                    DB::unprepared($query);
+                }
+            },
+            'rejected' => static function ($reason, $index) {
+            },
+        ]);
+
+        $promise = $pool->promise();
+        $promise->wait();
+    }
+}
