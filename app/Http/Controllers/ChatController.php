ee0045f5ae55ee2876c3b963495ba5ebb09c9df2
likunyan <lky216@gmail.com>
2021-02-09 10:27:17 +0800

    Update ChatController.php

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Events\TestBroadcastingEvent;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ChatController extends Controller
 {
-    public function message(Request $request): void
+    public function message(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'message' => ['required', 'max:255'],
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()])->setStatusCode(202);
+        }
+
         broadcast(new TestBroadcastingEvent($request->message))->toOthers();
     }
 }
