cc909f12abce25c77d32292130274d5233441297
likunyan <lky216@gmail.com>
2021-02-16 15:38:44 +0800

    Update TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class TaskController extends Controller
 {
@@ -12,23 +14,25 @@ class TaskController extends Controller
         $this->authorizeResource(Task::class, 'task');
     }
 
-    public function store(Request $request)
+    public function store(Project $project, Request $request)
     {
-        $validatedData = $request->validate(['title' => 'required']);
-
-        return Task::create([
-            'title' => $validatedData['title'],
-            'project_id' => $request->project_id,
-            'priority' => (int) $request->priority,
-            'is_completed' => 0,
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'priority' => ['nullable', Rule::in([1, 2, 3])],
         ]);
+
+        return $project->tasks()->save(new Task([
+            'title' => $validatedData['title'],
+            'priority' => $request->priority,
+        ]));
     }
 
     public function update(Task $task, Request $request)
     {
-        if ($request->input('priority') && ! in_array($request->input('priority'), [1, 2, 3, '低', '中', '高'])) {
-            return response()->json(['message' => '值在1（低）），2（中），3（高）范围'], 400);
-        }
+        $request->validate([
+            'is_completed' => ['nullable', 'boolean'],
+            'priority' => ['nullable', Rule::in([1, 2, 3])],
+        ]);
 
         $task->update($request->all());
 
