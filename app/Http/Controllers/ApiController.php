7ee5b2f00ac06ea0f2f7ca5b5105f775b98e3621
likunyan <lky216@gmail.com>
2021-03-10 09:45:10 +0800

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,16 +8,14 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Str;
-use JetBrains\PhpStorm\Pure;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 use TrueBV\Punycode;
 
 class ApiController extends Controller
 {
     public $guzzleClient;
 
-    public function index(): Collection | array
+    public function index(): Collection|array
     {
         return Api::all();
     }
@@ -81,79 +79,47 @@ class ApiController extends Controller
         return $domain;
     }
 
-    #[Pure]
- public function unicode_to_utf8($string): string
- {
-     $code = (int) hexdec($string);
-     //这里注意转换出来的 code 一定得是整形，这样才会正确的按位操作
-     $ord_1 = decbin(0xe0 | ($code >> 12));
-     $ord_2 = decbin(0x80 | (($code >> 6) & 0x3f));
-     $ord_3 = decbin(0x80 | ($code & 0x3f));
-
-     return chr(bindec($ord_1)).chr(bindec($ord_2)).chr(bindec($ord_3));
- }
-
-    #[Pure]
- public function utf8_to_unicode($string): string
- {
-     $unicode = (ord($string[0]) & 0x1F) << 12;
-     $unicode |= (ord($string[1]) & 0x3F) << 6;
-     $unicode |= (ord($string[2]) & 0x3F);
-
-     return '\u'.dechex($unicode);
- }
-
-    /**
-     * 随机获取一张图片作为登录背景.
-     */
-    public function random(): StreamedResponse
+    public function unicode_to_utf8($string): string
     {
-        $wallpaperFolder = public_path().'/images/wallpaper/';
-//        $wallpapers = \File::files($wallpaperFolder);
+        $code = (int) hexdec($string);
+        // 这里注意转换出来的 code 一定得是整形，这样才会正确的按位操作
+        $ord_1 = decbin(0xe0 | ($code >> 12));
+        $ord_2 = decbin(0x80 | (($code >> 6) & 0x3f));
+        $ord_3 = decbin(0x80 | ($code & 0x3f));
 
-        $wallpapers = [
-            'AIR.jpg',
-            'LiveForSpeed.jpg',
-            '你的名字.jpg',
-            '塞尔达荒野之息.jpg',
-            '守望先锋.jpg',
-            '骑士.jpeg',
-        ];
-
-        $random = random_int(0, count($wallpapers) - 1);
-        $wallpaperURL = $wallpaperFolder.$wallpapers[$random];
-
-//        $wallpaperURL = env('CDN_URL') . '/wallpaper/' . $wallpapers[$random];
-        return response()->stream(function () use ($wallpaperURL) {
-            echo file_get_contents($wallpaperURL);
-        }, 200, ['Content-Type' => 'image/jpeg']);
+        return chr(bindec($ord_1)).chr(bindec($ord_2)).chr(bindec($ord_3));
     }
 
-    #[Pure]
- public function base64_encode($string = ''): string
- {
-     return base64_encode($string);
- }
+    public function utf8_to_unicode($string): string
+    {
+        $unicode = (ord($string[0]) & 0x1F) << 12;
+        $unicode |= (ord($string[1]) & 0x3F) << 6;
+        $unicode |= (ord($string[2]) & 0x3F);
 
-    #[Pure]
- public function base64_decode($string = '')
- {
-     return base64_decode($string);
- }
+        return '\u'.dechex($unicode);
+    }
 
-    #[Pure]
- public function urlEncode($string = ''): string
- {
-     return urlencode($string);
- }
+    public function base64_encode($string = ''): string
+    {
+        return base64_encode($string);
+    }
 
-    #[Pure]
- public function urlDecode($string = ''): string
- {
-     return urldecode($string);
- }
+    public function base64_decode($string = '')
+    {
+        return base64_decode($string);
+    }
 
-    public function image($action = null): BinaryFileResponse | string | UrlGenerator | Application
+    public function urlEncode($string = ''): string
+    {
+        return urlencode($string);
+    }
+
+    public function urlDecode($string = ''): string
+    {
+        return urldecode($string);
+    }
+
+    public function image($action = null): BinaryFileResponse|string|UrlGenerator|Application
     {
         $uri = '/favicon.ico';
         switch ($action) {
@@ -164,49 +130,45 @@ class ApiController extends Controller
         }
     }
 
-    #[Pure]
- public function md5($string = ''): string
- {
-     return md5($string);
- }
+    public function md5($string = ''): string
+    {
+        return md5($string);
+    }
 
     public function userAgent()
     {
         return $_SERVER['HTTP_USER_AGENT'];
     }
 
-    #[Pure]
- public function sha($string): string
- {
-     return sha1($string);
- }
-
-    public function date($date = null): bool | int | string
+    public function sha($string): string
     {
-        return $date ? strtotime($date) : date('Y-m-d H:i:s', time());
+        return sha1($string);
     }
 
-    public function timestamp($timestamp = null): bool | int | string
+    public function date($date = null): bool|int|string
+    {
+        return $date ? strtotime($date) : date('Y-m-d H:i:s');
+    }
+
+    public function timestamp($timestamp = null): bool|int|string
     {
         return $timestamp ? date('Y-m-d H:i:s', $timestamp) : time();
     }
 
-    public function bankcard($cardNo): bool | string
+    public function bankcard($cardNo): bool|string
     {
         return file_get_contents('https://ccdcapi.alipay.com/validateAndCacheCardInfo.json?_input_charset=utf-8&cardNo='.$cardNo.'&cardBinCheck=true');
     }
 
-    #[Pure]
- public function secret($string = ''): string
- {
-     return str_repeat('*', strlen($string));
- }
+    public function secret($string = ''): string
+    {
+        return str_repeat('*', strlen($string));
+    }
 
-    #[Pure]
- public function hash($string = ''): string
- {
-     return sha1($string);
- }
+    public function hash($string = ''): string
+    {
+        return sha1($string);
+    }
 
     public function htmlSC($string): string
     {
@@ -228,11 +190,10 @@ class ApiController extends Controller
         return $_SERVER['REMOTE_ADDR'];
     }
 
-    #[Pure]
- public function howTime($content)
- {
-     return date('Y-m-d', strtotime($content));
- }
+    public function howTime($content)
+    {
+        return date('Y-m-d', strtotime($content));
+    }
 
     public function sp($content): string
     {
