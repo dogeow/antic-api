301b044d1c35b0c646c7459b23630c863877258b
likunyan <lky216@gmail.com>
2021-03-18 10:12:59 +0800

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -28,6 +28,7 @@ class ApiController extends Controller
      */
     public function callback(Request $request): array
     {
+        \Log::info(var_export($request->all(), true));
         return $request->all();
     }
 
