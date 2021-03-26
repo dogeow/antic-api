4508ca0140ea62b550ba9d528dc20b79f8d51f9e
likunyan <lky216@gmail.com>
2021-03-26 11:01:40 +0800

    Update PostCategoryController.php

--- app/Http/Controllers/PostCategoryController.php
+++ app/Http/Controllers/PostCategoryController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\PostCategory;
+use Illuminate\Support\Collection;
 
 class PostCategoryController extends Controller
 {
-    public function index()
+    public function index(): Collection
     {
         return PostCategory::groupBy('name')->get(['id', 'name']);
     }
