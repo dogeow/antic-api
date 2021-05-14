ea67a731e2b29ab89999aa19939804886be53007
likunyan <lky216@gmail.com>
2021-05-14 15:30:42 +0800

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -58,7 +58,7 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
-            'permission' => 0664,
+            'permission' => 0666,
         ],
 
         'slack' => [
