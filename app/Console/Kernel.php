cd5a05b1aa435bc899fc7373749edad7f0499877
likunyan <lky216@gmail.com>
2021-11-23 11:22:56 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('spider:date')->everyFiveMinutes()->withoutOverlapping();
+        $schedule->command('spider:date')->everyThirtyMinutes()->withoutOverlapping();
     }
 
     /**
