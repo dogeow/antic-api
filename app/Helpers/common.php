087e1d10db46e588d057f4f5db9eac38cebbb221
likunyan <lky216@gmail.com>
2022-02-11 17:09:22 +0800

    Update common.php

--- app/Helpers/common.php
+++ app/Helpers/common.php
@@ -84,3 +84,26 @@ function rgb2Hex(array|int $r, int $g = -1, int $b = -1): string
 
     return "#${color}";
 }
+
+/**
+ * @param  string  $number  需要转换的字符串
+ * @param  string  $targetBit  转成多少位
+ * @return string
+ */
+function baseConvert(string $number, string $targetBit): string
+{
+    $dic = [
+        0 => '0', 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9',
+        10 => 'A', 11 => 'B', 12 => 'C', 13 => 'D', 14 => 'E', 15 => 'F', 16 => 'G', 17 => 'H', 18 => 'I',
+        19 => 'J', 20 => 'K', 21 => 'L', 22 => 'M', 23 => 'N', 24 => 'O', 25 => 'P', 26 => 'Q', 27 => 'R',
+        28 => 'S', 29 => 'T', 30 => 'U', 31 => 'V', 32 => 'W', 33 => 'X', 34 => 'Y', 35 => 'Z',
+    ];
+    $remainder = bcmod($number, $targetBit);
+    $quotient = (string) floor((float) bcdiv($number, $targetBit));
+
+    if ($quotient === "0") {
+        return $dic[$remainder];
+    }
+
+    return baseConvert($quotient, $targetBit).$dic[$remainder];
+}
