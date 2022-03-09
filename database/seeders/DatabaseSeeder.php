1807725ab6411bba031a7b999681704e24485424
likunyan <lky216@gmail.com>
2022-03-09 10:08:53 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,12 +19,12 @@ class DatabaseSeeder extends Seeder
         $faker = app(Generator::class);
         User::create(
             [
-               'name' => 'test',
-               'email' => 'test@test.com',
-               'email_verified_at' => now(),
-               'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-               'remember_token' => Str::random(10),
-           ]
+                'name' => 'test',
+                'email' => 'test@test.com',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]
         );
 //         \App\Models\User::factory(1)->create();
 //         \App\Models\Post::factory(1)->create();
