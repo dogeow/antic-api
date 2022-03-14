0c49991a6a1669b83673580b014336a675c8cdbb
likunyan <lky216@gmail.com>
2022-03-14 23:32:06 +0800

    Update BroadcastServiceProvider.php

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,8 +14,8 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes(['middleware' => ['auth:api']]);
-//        Broadcast::routes(['middleware' => ['auth:sanctum']]);
+//        Broadcast::routes(['middleware' => ['auth:api']]);
+        Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
         require base_path('routes/channels.php');
     }
