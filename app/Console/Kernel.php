d122ff67beb1e4bbeeaf4039925e86704213c4e4
likunyan <lky216@gmail.com>
2022-04-05 17:08:57 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('spider:date')->everyMinute()->sendOutputTo('/root/spider-date.log');
+        $schedule->command('spider:date')->everyMinute();
     }
 
     /**
