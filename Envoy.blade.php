1f4114130250dd1afd8304a0c1560e49f87b8d83
likunyan <lky216@gmail.com>
2022-04-25 10:28:10 +0800

    Update Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,40 +1,47 @@
 @servers(['web' => 'root@dogeow.com', 'localhost' => '127.0.0.1'])
 
 @setup
-$path = '/var/www/antic-api';
-$now = new DateTime();
-$environment = isset($env) ? $env : 'testing';
+    $path = '/var/www/antic-api';
+    $now = new DateTime();
+    $environment = isset($env) ? $env : 'testing';
 @endsetup
 
 @story('deploy', ['on' => 'web'])
-git
-composer
-laravel
-lighthouse
+    git
+    composer
+    laravel
+    lighthouse
 @endstory
 
 @task('git')
-cd {{$path}}
-git pull
+    cd {{$path}}
+    git pull
 @endtask
 
 @task('composer')
-cd {{$path}}
-sudo -u www-data composer install --no-plugins --no-scripts
+    cd {{$path}}
+    sudo -u www-data composer install --no-plugins --no-scripts
 @endtask
 
 @task('laravel')
-cd {{$path}}
-php artisan migrate --force
-php artisan optimize
+    cd {{$path}}
+    php artisan migrate --force
+    php artisan optimize
 @endtask
 
 @task('lighthouse')
-cd {{$path}}
-php artisan lighthouse:clear-cache
-php artisan lighthouse:cache
+    cd {{$path}}
+    php artisan lighthouse:clear-cache
+    php artisan lighthouse:cache
 @endtask
 
-@after
-echo "\r\n新版本发布完成！\r\n";
-@endafter
+@finished
+    echo PHP_EOL;
+    if ($exitCode > 0) {
+        echo "有一些错误！";
+    } else {
+        echo "新版本发布完成！";
+    }
+@endfinished
+
+
