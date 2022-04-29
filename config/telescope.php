5d77a919bd068adc63f25223cb6c4fa6587fc0aa
likunyan <lky216@gmail.com>
2022-04-29 10:51:39 +0800

    Update telescope.php

--- config/telescope.php
+++ config/telescope.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\AppendToken;
 use Laravel\Telescope\Http\Middleware\Authorize;
 use Laravel\Telescope\Watchers;
 
@@ -77,7 +76,7 @@ return [
     */
 
     'middleware' => [
-        AppendToken::class,
+//        AppendToken::class,
         'auth:sanctum',
         Authorize::class,
     ],
