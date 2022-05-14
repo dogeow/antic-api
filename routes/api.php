dd5800ec9930a91602c3d2bbc1045178e924e5c4
likunyan <lky216@gmail.com>
2022-05-14 23:23:48 +0800

    Revert "Update api.php"
    This reverts commit b1d28db2910634d279ad56c699f4959e9c0cf582.

--- routes/api.php
+++ routes/api.php
@@ -106,36 +106,34 @@ Route::post('images', [ImageController::class, 'store']);
 // Search
 Route::get('search', [SearchController::class, 'search']);
 
-Route::controller(ApiController::class)->group(function () {
-    // 便民 API
-    Route::post('api', 'index');
-    Route::get('html_sc/{string}', 'htmlSC');
-    Route::get('secret/{string?}', 'secret');
-    // 图片
-    Route::get('images', 'images');
-    // url
-    Route::get('url_decode/{string?}', 'urlDecode');
-    Route::get('url_encode/{string?}', 'urlEncode');
-    Route::get('base64_encode/{string?}', 'base64_encode');
-    Route::get('base64_decode/{string?}', 'base64_decode');
-    Route::get('utf8_to_unicode/{string}', 'utf8_to_unicode');
-    Route::get('unicode_to_utf8/{string}', 'unicode_to_utf8');
-    Route::get('punycode/{string?}', 'punycode');
-    Route::get('image/{action}', 'image');
-    Route::get('md5/{string?}', 'md5');
-    Route::get('user-agent', 'userAgent');
-    Route::get('hash/{string?}', 'hash');
-    Route::get('ip/{ip?}', 'ip')
-        ->where(['ip' => '^((2(5[0-5]|[0-4]\d))|[0-1?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}$']);
-    // 时间
-    Route::get('date/{timestamp?}', 'date')->where(['timestamp' => '[0-9]{1,10}']);
-    Route::get('datetime/{timestamp?}', 'datetime')->where(['timestamp' => '[0-9]{1,10}']);
-    Route::get('timestamp/{date?}', 'timestamp')->where(['date' => '[0-9-\ :]+']);
-    Route::get('bankcard/{cardNo}', 'bankcard')->where(['cardNo' => '[0-9]+']);
-    Route::get('case/{string}', 'case');
-    Route::post('url-title', 'getTitle');
-});
-
+// 便民 API
+Route::post('api', [ApiController::class, 'index']);
+Route::get('html_sc/{string}', [ApiController::class, 'htmlSC']);
+Route::get('secret/{string?}', [ApiController::class, 'secret']);
+// 图片
+Route::get('images', [ApiController::class, 'images']);
+// url
+Route::get('url_decode/{string?}', [ApiController::class, 'urlDecode']);
+Route::get('url_encode/{string?}', [ApiController::class, 'urlEncode']);
+Route::get('base64_encode/{string?}', [ApiController::class, 'base64_encode']);
+Route::get('base64_decode/{string?}', [ApiController::class, 'base64_decode']);
+Route::get('utf8_to_unicode/{string}', [ApiController::class, 'utf8_to_unicode']);
+Route::get('unicode_to_utf8/{string}', [ApiController::class, 'unicode_to_utf8']);
+Route::get('punycode/{string?}', [ApiController::class, 'punycode']);
+Route::get('image/{action}', [ApiController::class, 'image']);
+Route::get('md5/{string?}', [ApiController::class, 'md5']);
+Route::get('user-agent', [ApiController::class, 'userAgent']);
+Route::get('hash/{string?}', [ApiController::class, 'hash']);
+Route::get('ip/{ip?}', [
+    ApiController::class, 'ip',
+])->where(['ip' => '^((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}$']);
+// 时间
+Route::get('date/{timestamp?}', [ApiController::class, 'date'])->where(['timestamp' => '[0-9]{1,10}']);
+Route::get('datetime/{timestamp?}', [ApiController::class, 'datetime'])->where(['timestamp' => '[0-9]{1,10}']);
+Route::get('timestamp/{date?}', [ApiController::class, 'timestamp'])->where(['date' => '[0-9-\ :]+']);
+Route::get('bankcard/{cardNo}', [ApiController::class, 'bankcard'])->where(['cardNo' => '[0-9]+']);
+Route::get('case/{string}', [ApiController::class, 'case']);
+Route::post('url-title', [ApiController::class, 'getTitle']);
 Route::get('keywords/{content}', [JiebaController::class, 'keywords']);
 
 // 书签
