7a1f3d0aeae570b74f662817fe0481be6ea33541
likunyan <lky216@gmail.com>
2023-03-23 09:58:01 +0800

    Create FilterKeyword.php

---
+++ app/Console/Commands/FilterKeyword.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FilterKeyword extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'filter:keyword';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        $arr = [
+            "雪山",
+            "大峡谷",
+            "长城",
+        ];
+
+        $arr = array_unique($arr);
+
+        // 判断字符，大于三个中文的不要，小于两个字的也不要
+        foreach ($arr as $key => $value) {
+            $skip = false;
+
+            $len = mb_strlen($value);
+            if ($len > 3) {
+                $value = preg_replace('/(.*?)(水库|湖泊|保护区)$/', '$1', $value);
+            }
+
+            $len = mb_strlen($value);
+            if ($len == 3 || $len == 2) {
+                echo $value.PHP_EOL;
+            }
+        }
+    }
+}
