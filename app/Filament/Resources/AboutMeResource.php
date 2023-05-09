7bc49a1f6b66431e1abf3a875ab1d60f55ee400e
likunyan <lky216@gmail.com>
2023-05-09 10:46:47 +0800

    Update AboutMeResource.php

--- app/Filament/Resources/AboutMeResource.php
+++ app/Filament/Resources/AboutMeResource.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\AboutMeResource\Pages;
-use App\Models\About;
+use App\Models\AboutMe;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
@@ -12,7 +12,7 @@ use Filament\Tables;
 
 class AboutMeResource extends Resource
 {
-    protected static ?string $model = About::class;
+    protected static ?string $model = AboutMe::class;
 
     protected static ?string $navigationLabel = '关于我';
 
