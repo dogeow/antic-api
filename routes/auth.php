12a83e46eb944cd5205532093d07fa45f31e564f
likunyan <lky216@gmail.com>
2023-05-10 10:57:57 +0800

    233

--- routes/auth.php
+++ routes/auth.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Middleware\Auth\AuthenticatedSessionController;
+use App\Http\Middleware\Auth\EmailVerificationNotificationController;
+use App\Http\Middleware\Auth\NewPasswordController;
+use App\Http\Middleware\Auth\PasswordResetLinkController;
+use App\Http\Middleware\Auth\RegisteredUserController;
+use App\Http\Middleware\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
