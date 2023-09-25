2cc0d23c3dfea87b0dcea6047ae32e2f3b467233
likunyan <lky216@gmail.com>
2023-09-25 14:54:19 +0800

    变更

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -413,7 +413,7 @@ class ApiController extends Controller
         ];
 
         // https://api.openai.com/v1/chat/completions
-        $response = $client->get('https://openkey.cloud', [
+        $response = $client->post('https://openkey.cloud/v1/chat/completions', [
             'headers' => $headers,
             'json' => $body,
         ]);
